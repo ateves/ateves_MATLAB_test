d98f6ca96eac31e97aab0b557ea912d195c3598e
ateves <39039918+ateves@users.noreply.github.com>
2025-12-11 17:54:07 -0500

    Initial Creation
    We are looking at a simple matlab file with comments.

---
+++ anthony.m
@@ -0,0 +1,5 @@
+% This is a simple script to add numbers
+a = 5; % Define a variable
+b = 10; % Define another variable
+total = a + b; % Perform a calculation
+disp(total); % Display the result (outputs 15)
